41681a17360c64104fd7a0379f0d069132c52e68
ignatov-dmitry <ignatov.d43@gmail.com>
2017-05-31 12:12:16 +0300

    add project

---
+++ CTI/App_Start/BundleConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace CTI
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery.js",
+                        "~/Scripts/jquery-ui.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/rs-plugin").Include(
+                        "~/Scripts/jquery.themepunch.tools.min.js",
+                        "~/Scripts/jquery.themepunch.revolution.min.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                        "~/Scripts/prettify.js",
+                        "~/Scripts/scripts.js",
+                        "~/Scripts/custom.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/css/scripts.css",
+                      "~/Content/rs-plugin/css/settings.css",
+                      "~/Content/css/core-icons/core_style.css",
+                      "~/Content/css/icons/style.css",
+                      "~/Content/css/style.css"));
+        }
+    }
+}
